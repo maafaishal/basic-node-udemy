fd68d54faae1033c8e79d3ed59400c9de5d14e7a
Muhammad A Faishal <maafaishal@gmail.com>
2024-08-15 20:17:21 +0700

    refactor: sharing data

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -3,14 +3,16 @@ const path = require("node:path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../views/add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log("req.body", req.body);
-
+  products.push(req.body);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("node:path");
 
+const adminRoutes = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log("adminRoutes.products", adminRoutes.products);
   res.sendFile(path.join(__dirname, "../views/shop.html"));
 });
 
