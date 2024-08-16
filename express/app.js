c0c9c39b52d2d51e11e80211583cbbcc9e165ce8
Muhammad A Faishal <maafaishal@gmail.com>
2024-08-16 09:27:35 +0700

    feat: ejs views

--- express/app.js
+++ express/app.js
@@ -2,6 +2,7 @@ const http = require("node:http");
 const path = require("node:path");
 
 const express = require("express");
+// const { engine } = require("express-handlebars");
 
 const bodyParser = require("body-parser");
 
@@ -9,6 +10,17 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
+
+// app.engine(
+//   "hbs",
+//   engine({
+//     extname: "hbs",
+//     layoutsDir: "express/views/layouts",
+//     defaultLayout: "main-layout",
+//   })
+// );
+app.set("view engine", "ejs");
+app.set("views", "express/views");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("node:path");
 
-const adminRoutes = require("./admin");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("adminRoutes.products", adminRoutes.products);
-  res.sendFile(path.join(__dirname, "../views/shop.html"));
+  // console.log("adminRoutes.products", adminRoutes.products);
+  // res.sendFile(path.join(__dirname, "../views/shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    pageTitle: "Product List",
+    products,
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
